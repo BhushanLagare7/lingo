c0cb4e06b13b11c157997c1276b135b7d9d3f28b
Bhushan Lagare <bhushanlagare7@gmail.com>
2026-04-25 12:16:31 +0530

    feat(marketing): add language flags to footer and public assets

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,62 @@
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
 export const Footer = () => {
   return (
     <footer className="hidden p-2 w-full h-20 border-t-2 lg:block border-slate-200">
-      Footer
+      <div className="flex justify-evenly items-center mx-auto max-w-5xl h-full">
+        <Button className="flex-1" size="lg" variant="ghost">
+          <Image
+            alt="Croatian"
+            className="mr-4 rounded-md"
+            height={32}
+            src="/hr.svg"
+            width={40}
+          />
+          Croatian
+        </Button>
+        <Button className="flex-1" size="lg" variant="ghost">
+          <Image
+            alt="Spanish"
+            className="mr-4 rounded-md"
+            height={32}
+            src="/es.svg"
+            width={40}
+          />
+          Spanish
+        </Button>
+        <Button className="flex-1" size="lg" variant="ghost">
+          <Image
+            alt="French"
+            className="mr-4 rounded-md"
+            height={32}
+            src="/fr.svg"
+            width={40}
+          />
+          French
+        </Button>
+        <Button className="flex-1" size="lg" variant="ghost">
+          <Image
+            alt="Italian"
+            className="mr-4 rounded-md"
+            height={32}
+            src="/it.svg"
+            width={40}
+          />
+          Italian
+        </Button>
+        <Button className="flex-1" size="lg" variant="ghost">
+          <Image
+            alt="Japanese"
+            className="mr-4 rounded-md"
+            height={32}
+            src="/jp.svg"
+            width={40}
+          />
+          Japanese
+        </Button>
+      </div>
     </footer>
   );
 };
